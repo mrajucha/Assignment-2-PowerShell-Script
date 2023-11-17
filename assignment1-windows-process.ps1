92dff8758cd060f919e3ce158158db0609ab01a9
Manoj <manoj.raju-chaudhary@capgemini.com>
2023-11-17 13:29:25 +0530

    assignment 2 is added

--- assignment1-windows-process.ps1
+++ assignment1-windows-process.ps1
@@ -2,13 +2,13 @@
 
 # Additionally added the sort-object in descending.
 
-# Step 1: Retrieve a list of running processes
+# Retrieve a list of running processes
 $processes = Get-Process
 
-# Step 2: Filter processes to display only those with high CPU utilization (above 80%)
+#  Filter processes to display only those with high CPU utilization (above 80%)
 $highCPUProcesses = $processes | Where-Object { $_.CPU -gt 80 } | Sort-Object CPU -Descending
 
-# Step 3: Save the list of high CPU utilization processes to a log file with the current date and time
+# : Save the list of high CPU utilization processes to a log file with the current date and time
 $logFilePath = "HighCPUProcesses_$(Get-Date -Format 'yyyyMMdd_HHmmss').log"
 $highCPUProcesses | Format-Table -AutoSize | Out-File -FilePath $logFilePath
 
